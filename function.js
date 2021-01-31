b3abbf65ac0d0c29b9b44d0f110ccc96c76ef6b4
mhmdsalti <alsalty_r@hotmail.com>
2021-01-31 15:34:54 +0200

    m

---
+++ function.js
@@ -0,0 +1,24 @@
+
+function getName() {
+    var userName = prompt('Enter your name?');
+    console.log(typeof prompt);
+    console.log(userName);
+    document.write(userName);
+    for (var i = 0; i <= 10; i++) {
+      console.log(i);
+    }
+  };
+  
+  
+  getName();
+
+
+ 
+  
+   
+  
+  
+  
+
+
+  
